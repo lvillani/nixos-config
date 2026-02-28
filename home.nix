6e2feaa690089ddd8d686cbd1c91ee85b169c9ba
Lorenzo Villani <lorenzo@villani.me>
2026-02-28 19:20:57 +0100

    feat: add vscode snippets

--- home.nix
+++ home.nix
@@ -1,12 +1,24 @@
 { pkgs, ... }:
 let
   homeDirectory = if pkgs.stdenv.hostPlatform.isDarwin then /Users/user else /home/user;
+  vscodeUserDirectory =
+    if pkgs.stdenv.hostPlatform.isDarwin then
+      "Library/Application Support/Code/User"
+    else
+      ".config/Code/User";
 in
 {
   home.stateVersion = "25.11";
 
   home.homeDirectory = homeDirectory;
   home.username = "user";
+
+  home.file = {
+    "${vscodeUserDirectory}/snippets" = {
+      source = ./files/vscode/snippets;
+      recursive = true;
+    };
+  };
 
   programs.home-manager.enable = true;
 
