e75429091bd4235d23474632feef61f18fa1ff21
Lorenzo Villani <lorenzo@villani.me>
2026-02-28 15:35:04 +0100

    fix: use absolute path type for home directory

--- home.nix
+++ home.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 let
-  homeDirectory = if pkgs.stdenv.hostPlatform.isDarwin then "/Users/user" else "/home/user";
+  homeDirectory = if pkgs.stdenv.hostPlatform.isDarwin then /Users/user else /home/user;
 in
 {
   home.stateVersion = "25.11";
