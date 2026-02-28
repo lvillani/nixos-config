69c2bfc6910b9bbce6794b3ae426ce365b4dd6db
Lorenzo Villani <lorenzo@villani.me>
2026-02-28 12:09:13 +0100

    chore: add nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, ... }@inputs: { };
