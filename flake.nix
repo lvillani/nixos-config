eccfaac52a063eabf5b19f8833da014e69b2ccc3
Lorenzo Villani <lorenzo@villani.me>
2026-03-01 18:43:34 +0100

    chore: remove currently unused nixpkgs unstable input

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/*";
     determinate.inputs.nixpkgs.follows = "nixpkgs";
