9ecef745c89717abb866e7e1f57b784464cfb029
Lorenzo Villani <lorenzo@villani.me>
2026-03-02 18:29:00 +0100

    feat: add nixos boot options

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,14 @@
 { ... }:
 {
   system.stateVersion = "25.11";
+
+  boot.initrd.systemd.enable = true;
+
+  boot.kernel.sysctl."kernel.sysrq" = 1;
+  boot.kernelParams = [ "quiet" ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.timeout = 0;
+
+  boot.plymouth.enable = true;
 }
