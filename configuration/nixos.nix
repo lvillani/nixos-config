a2347b71e4d8b8a72949d2146a9509f1df7b2669
Lorenzo Villani <lorenzo@villani.me>
2026-04-09 10:04:52 +0200

    feat(nixos): enable filterForward in firewall configuration

--- configuration/nixos.nix
+++ configuration/nixos.nix
@@ -14,6 +14,7 @@
 
   networking.nftables.enable = true;
 
+  networking.firewall.filterForward = true;
   networking.firewall.logRefusedPackets = true;
   networking.firewall.logReversePathDrops = true;
 
