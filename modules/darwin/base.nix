a89d979a3bdd6e4f3d0732e8b818c498d32ee38a
Lorenzo Villani <lorenzo@villani.me>
2026-04-23 21:43:37 +0200

    refactor: make base-darwin host a thin wrapper over module
    This makes the base Darwin configuration match other NixOS base
    configurations.

---
+++ modules/darwin/base.nix
@@ -0,0 +1,58 @@
+{
+  inputs,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  imports = [ inputs.self.modules.nixos-and-darwin.shared ];
+
+  environment.systemPackages = with pkgs; [
+    appcleaner
+    coconutbattery
+  ];
+
+  homebrew.enable = true;
+  homebrew.brews = [ "mas" ];
+  homebrew.onActivation.cleanup = "zap";
+
+  networking.applicationFirewall.enable = true;
+  networking.applicationFirewall.allowSigned = true;
+  networking.applicationFirewall.allowSignedApp = false;
+
+  nixpkgs.hostPlatform = lib.mkDefault "aarch64-darwin";
+
+  security.pam.services.sudo_local.touchIdAuth = true;
+
+  system.defaults.dock.autohide = true;
+  system.defaults.dock.autohide-delay = 0.0;
+  system.defaults.dock.mru-spaces = false;
+
+  system.defaults.finder._FXSortFoldersFirst = true;
+  system.defaults.finder._FXSortFoldersFirstOnDesktop = true;
+  system.defaults.finder.FXDefaultSearchScope = "SCcf"; # Search current folder
+  system.defaults.finder.FXPreferredViewStyle = "clmv"; # Column view
+  system.defaults.finder.NewWindowTarget = "Home"; # Home directory
+
+  system.defaults.CustomSystemPreferences = {
+    "com.apple.Safari" = {
+      AutoFillCreditCardData = false;
+      AutoFillFromAddressBook = false;
+      AutoFillMiscellaneousForms = false;
+      AutoFillPasswords = false;
+      AutoOpenSafeDownloads = false;
+      SearchProviderIdentifier = "com.duckduckgo";
+      ShowFullURLInSmartSearchField = true;
+    };
+    "com.apple.TextEdit" = {
+      RichText = false;
+    };
+  };
+
+  system.primaryUser = "user";
+
+  users.users.user = {
+    home = "/Users/user";
+    shell = pkgs.fish;
+  };
+}
