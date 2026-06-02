694c6af1979ae93f5692003ad978656e9e2a1c66
Lorenzo Villani <lorenzo@villani.me>
2026-06-02 19:09:11 +0200

    fix: workaround for 1password-gui hash mismatch

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,7 +1,19 @@
 { inputs }:
-final: prev: {
+final: prev:
+let
+  isDarwinAarch64 = prev.stdenv.hostPlatform.isDarwin && prev.stdenv.hostPlatform.isAarch64;
+in
+{
   unstable = import inputs.nixpkgs-unstable {
     inherit (prev.stdenv.hostPlatform) system;
     inherit (prev) config;
   };
+}
+// prev.lib.optionalAttrs isDarwinAarch64 {
+  _1password-gui = prev._1password-gui.overrideAttrs (_: {
+    src = prev.fetchurl {
+      url = "https://downloads.1password.com/mac/1Password-8.12.21-aarch64.zip";
+      hash = "sha256-WrWbGzBK65tVNl9Dc3OnJURiPpfbNLOYUJcVT0ETaAs=";
+    };
+  });
 }
