faec06bb24d462468a6c37dded07a249d481e659
Lorenzo Villani <lorenzo@villani.me>
2026-06-11 19:00:41 +0200

    refactor: vscode overlay (and update to 1.124.0)

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,57 +2,38 @@
 final: prev:
 let
   system = prev.stdenv.hostPlatform.system;
-  isDarwinAarch64 = system == "aarch64-darwin";
-  isLinuxX86_64 = system == "x86_64-linux";
+
+  vscodeVersion = "1.124.0";
+  vscodeSystem = {
+    aarch64-darwin = "darwin-arm64";
+    x86_64-linux = "linux-x64";
+  };
+  vscodeSrc = {
+    aarch64-darwin = {
+      name = "VSCode_${vscodeVersion}_darwin-arm64.zip";
+      url = "https://update.code.visualstudio.com/${vscodeVersion}/darwin-arm64/stable";
+      hash = "sha256-9AC7xsB7Wj4WTN7Z0i3H/W/DP54ttPc0wGPNrBFGXrk=";
+    };
+    x86_64-linux = {
+      name = "VSCode_${vscodeVersion}_linux-x64.tar.gz";
+      url = "https://update.code.visualstudio.com/${vscodeVersion}/linux-x64/stable";
+      hash = "sha256-eUiOpSJCcdDeSk3NRv4fxN3RSrG4axGB6U5zh5DEZXc=";
+    };
+  };
 in
 {
   unstable = import inputs.nixpkgs-unstable {
     inherit (prev.stdenv.hostPlatform) system;
     inherit (prev) config;
   };
-}
-// prev.lib.optionalAttrs (isDarwinAarch64 || isLinuxX86_64) {
-  # 1.123.0 is not yet in nixpkgs. Since 1.122, VSCode switched from
-  # @vscode/ripgrep to @vscode/ripgrep-universal; patch postPatch accordingly.
-  # TODO: rebase on nixpkgs-unstable's vscode recipe once 1.222+ lands on nixos-unstable.
-  vscode = prev.vscode.overrideAttrs (
-    old:
-    let
-      version = "1.123.0";
-      plat =
-        {
-          x86_64-linux = "linux-x64";
-          aarch64-darwin = "darwin-arm64";
-        }
-        .${system};
-      archive_fmt = if prev.stdenv.hostPlatform.isDarwin then "zip" else "tar.gz";
-      hash =
-        {
-          x86_64-linux = "sha256-L975R3F779LgaFTL4B6ZtImPd1LyXhImnDgCPmO5PI8=";
-          aarch64-darwin = "sha256-AY6WeDzGEH5zXRosN1H/osxC3e5j0Hs9s2Ys2xe1UxI=";
-        }
-        .${system};
-      oldRipgrepPath =
-        {
-          x86_64-linux = "resources/app/node_modules/@vscode/ripgrep/bin/rg";
-          aarch64-darwin = "Contents/Resources/app/node_modules/@vscode/ripgrep/bin/rg";
-        }
-        .${system};
-      newRipgrepPath =
-        {
-          x86_64-linux = "resources/app/node_modules/@vscode/ripgrep-universal/bin/linux-x64/rg";
-          aarch64-darwin = "Contents/Resources/app/node_modules/@vscode/ripgrep-universal/bin/darwin-arm64/rg";
-        }
-        .${system};
-    in
-    {
-      inherit version;
-      src = prev.fetchurl {
-        name = "VSCode_${version}_${plat}.${archive_fmt}";
-        url = "https://update.code.visualstudio.com/${version}/${plat}/stable";
-        inherit hash;
-      };
-      postPatch = builtins.replaceStrings [ oldRipgrepPath ] [ newRipgrepPath ] old.postPatch;
-    }
-  );
+
+  vscode = prev.vscode.overrideAttrs (previousAttrs: {
+    version = vscodeVersion;
+    src = prev.fetchurl vscodeSrc.${system};
+    postPatch =
+      builtins.replaceStrings
+        [ "@vscode/ripgrep/bin/rg" ]
+        [ "@vscode/ripgrep-universal/bin/${vscodeSystem.${system}}/rg" ]
+        previousAttrs.postPatch;
+  });
 }
