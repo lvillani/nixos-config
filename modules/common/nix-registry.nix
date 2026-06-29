874f1e9c1fc54e7c80df1f86ca6cdc098de78493
Lorenzo Villani <lorenzo@villani.me>
2026-06-29 21:17:00 +0200

    feat: add n and nu aliases for nixpkgs and nixpkgs-unstable registries

--- modules/common/nix-registry.nix
+++ modules/common/nix-registry.nix
@@ -2,4 +2,13 @@
 {
   nix.registry.nixpkgs.flake = inputs.nixpkgs;
   nix.registry.nixpkgs-unstable.flake = inputs.nixpkgs-unstable;
+
+  nix.registry.n.to = {
+    type = "indirect";
+    id = "nixpkgs";
+  };
+  nix.registry.nu.to = {
+    type = "indirect";
+    id = "nixpkgs-unstable";
+  };
 }
