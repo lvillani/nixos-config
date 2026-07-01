8fcdddf9ea8d30c5fc6d4ad5c653fb9a25713891
Lorenzo Villani <lorenzo@villani.me>
2026-07-01 22:21:59 +0200

    chore: update vscode to version 1.127.0

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,7 +3,7 @@ final: prev:
 let
   system = prev.stdenv.hostPlatform.system;
 
-  vscodeVersion = "1.125.0";
+  vscodeVersion = "1.127.0";
   vscodeSystem = {
     aarch64-darwin = "darwin-arm64";
     x86_64-linux = "linux-x64";
@@ -12,12 +12,12 @@ let
     aarch64-darwin = {
       name = "VSCode_${vscodeVersion}_darwin-arm64.zip";
       url = "https://update.code.visualstudio.com/${vscodeVersion}/darwin-arm64/stable";
-      hash = "sha256-dMSYvcryDd8Wqu8GHwnrp5pyRJwLzpV9BDLnpyWcxG0=";
+      hash = "sha256-IHu9EwW9/oS2FTr/mB7ugMss5Pku3IyslqFYr4riZyk=";
     };
     x86_64-linux = {
       name = "VSCode_${vscodeVersion}_linux-x64.tar.gz";
       url = "https://update.code.visualstudio.com/${vscodeVersion}/linux-x64/stable";
-      hash = "sha256-TTulHpCiT2eay2tb7e1ub164rgttBnB36Cc4JVoxf08=";
+      hash = "sha256-4G+zZ5HJuvdJXUt9wPWqqCVOfRpgpe5D5sfevAXJYrU=";
     };
   };
 in
