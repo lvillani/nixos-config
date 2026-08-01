7199b95795516fe26390e83c2a6c93fc26faf39a
Lorenzo Villani <lorenzo@villani.me>
2026-08-01 08:11:57 +0200

    feat(vscode): update to 1.131

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,17 +3,17 @@ final: prev:
 let
   system = prev.stdenv.hostPlatform.system;
 
-  vscodeVersion = "1.130.0";
+  vscodeVersion = "1.131.0";
   vscodeSrc = {
     aarch64-darwin = {
       name = "VSCode_${vscodeVersion}_darwin-arm64.zip";
       url = "https://update.code.visualstudio.com/${vscodeVersion}/darwin-arm64/stable";
-      hash = "sha256-bhbMscqsOU2ux4i2XShdMKgJPN8tuWVSxTzJ0CUvJNM=";
+      hash = "sha256-eWw64c0o1Fs/uEUMD4Zhzy9DYy46Dzj1Al8MSWdbz5k=";
     };
     x86_64-linux = {
       name = "VSCode_${vscodeVersion}_linux-x64.tar.gz";
       url = "https://update.code.visualstudio.com/${vscodeVersion}/linux-x64/stable";
-      hash = "sha256-fWrT06eKxFUcFGMfeNfgPIUoKrUFw86LG8BOAfr+iOo=";
+      hash = "sha256-hjLm+a7XosVhJHVVn/PRjGYdNUMsImUG23BESpmEVzE=";
     };
   };
 in
