fe58d5f9406d94bd6edff1d8047a123779a44ccd
Lorenzo Villani <lorenzo@villani.me>
2026-08-14 13:19:34 +0200

    refactor(vscode): use unstable package directly

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,32 +1,12 @@
 { inputs }:
 final: prev:
-let
-  system = prev.stdenv.hostPlatform.system;
-
-  vscodeVersion = "1.133.0";
-  vscodeSrc = {
-    aarch64-darwin = {
-      name = "VSCode_${vscodeVersion}_darwin-arm64.zip";
-      url = "https://update.code.visualstudio.com/${vscodeVersion}/darwin-arm64/stable";
-      hash = "sha256-KxP/IfZArzsb6c8sJn/V8BdfZwBOxuEmNTVgDzgr9C0=";
-    };
-    x86_64-linux = {
-      name = "VSCode_${vscodeVersion}_linux-x64.tar.gz";
-      url = "https://update.code.visualstudio.com/${vscodeVersion}/linux-x64/stable";
-      hash = "sha256-K/GpDS8AivAJ6zxKe9CEm5JHoFiM05pASgseaRvmgWE=";
-    };
-  };
-in
 {
   unstable = import inputs.nixpkgs-unstable {
     inherit (prev.stdenv.hostPlatform) system;
     inherit (prev) config;
   };
 
-  vscode = final.unstable.vscode.overrideAttrs (previousAttrs: {
-    version = vscodeVersion;
-    src = final.unstable.fetchurl vscodeSrc.${system};
-  });
+  vscode = final.unstable.vscode;
 
   pi-coding-agent = final.unstable.pi-coding-agent;
 }
